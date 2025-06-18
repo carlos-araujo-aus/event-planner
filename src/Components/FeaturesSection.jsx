e55d9db72de126d4e7ac9e116363d66147ba6317
Carlos Araujo <carlos.araujo.aus@gmail.com>
2025-06-18 20:24:18 +0800

    new components added and EventPlanner jxs and css modified

---
+++ src/Components/FeaturesSection.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './EventPlanner.css';
+
+const FeaturesSection = () => {
+    return (
+        <>
+        <section className='features'>
+
+            <h2>Features</h2>
+
+            <ul>
+                <li>Easy event creation and management</li>
+                <li>Customizable event templates</li>
+                <li>Guest list management</li>
+                <li>Real-time collaboration</li>
+                <li>Reminders and notifications</li>
+            </ul>
+
+        </section>
+
+        </>
+    )
+}
+
+export default FeaturesSection;
---
+++ src/Components/TestimonialsSection.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './EventPlanner.css';
+
+const TestimonialsSection = () => {
+    return (
+        <>
+            <section className="testimonials">
+
+                <div className="testimonial">
+
+                    <p>&quot;Event Planner made organizing my wedding a breeze. Highly recommended!&quot;</p>
+                    <p className="author">- Emily Johnson</p>
+
+                </div>
+                <div className="testimonial">
+
+                    <p>&quot;I use Event Planner for all my corporate events. It saves me so much time and effort!!&quot;</p>
+                    <p className="author">- John Smith</p>
+
+                </div>
+
+            </section>
+
+        </>
+    )
+}
+
+export default TestimonialsSection;
